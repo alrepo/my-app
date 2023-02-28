46406be2aa7665b933db3c0f3d951a99e9d22bc6
braintalked <70242053+alrepo@users.noreply.github.com>
2023-02-28 09:11:27 +0300

    Updates

---
+++ precache-manifest.42e9570f6679cde84d65f7be2cfd3af0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f6eca41a60f612febd6b81b4e761ee",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c04c15ae06c91abe156a",
+    "url": "/my-app/static/js/2.ae2546fe.chunk.js"
+  },
+  {
+    "revision": "160ca1751741f6ef8e85",
+    "url": "/my-app/static/js/main.457feeb8.chunk.js"
+  },
+  {
+    "revision": "149c1212c619427c0519",
+    "url": "/my-app/static/js/runtime~main.9dd19628.js"
+  }
+]);
