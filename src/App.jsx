b485b8d7f96ee8135fe4159c5a4247fc2add5aa9
braintalked <70242053+alrepo@users.noreply.github.com>
2023-02-28 08:32:42 +0300

    111

---
+++ src/App.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import JobCard from "./JobCard";
+
+let jobs = [
+  {
+    jobTitle: "طباخ خبير",
+    companyName: "مطاعم ماكدونلدز",
+    companyLogo: "https://pbs.twimg.com/profile_images/1625792557291651073/GDxGcTsc_400x400.png",
+    location: "الرياض",
+    salary: "50-70 ألف ريال",
+    filter1: "طباخ",
+    filter2: "فاين دايننق",
+    cardOptions: {fixed: 0, highlight: false, showLogo: true}
+  },
+  {
+    jobTitle: "Opration Manager",
+    companyName: "Rabbit Restaurant",
+    companyLogo: "https://img.freepik.com/premium-vector/rabbit-cartoon-logo-design-vector-illustration_256145-308.jpg?w=2000",
+    location: "Makka",
+    salary: "SAR 100k-150k",
+    filter1: "Manager",
+    filter2: "Fast Food",
+    cardOptions: {fixed: 1, highlight: true, showLogo: true}
+  },
+  {
+    jobTitle: "Barista",
+    companyName: "Starbucks Coffee",
+    companyLogo: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/640px-Starbucks_Corporation_Logo_2011.svg.png",
+    location: "Madina",
+    salary: null,
+    filter1: "Barista",
+    filter2: "Drive Thru",
+    cardOptions: {fixed: 0, highlight: false, showLogo: false}
+  },
+  {
+    jobTitle: "طباخ خبير",
+    companyName: "مطاعم ماكدونلدز",
+    companyLogo: "https://pbs.twimg.com/profile_images/1625792557291651073/GDxGcTsc_400x400.png",
+    location: "الرياض",
+    salary: "50-70 ألف ريال",
+    filter1: "طباخ",
+    filter2: "فاين دايننق",
+    cardOptions: {fixed: 0, highlight: false, showLogo: true}
+  },
+  {
+    jobTitle: "طباخ خبير",
+    companyName: "مطاعم ماكدونلدز",
+    companyLogo: "https://pbs.twimg.com/profile_images/1625792557291651073/GDxGcTsc_400x400.png",
+    location: "الرياض",
+    salary: "50-70 ألف ريال",
+    filter1: "طباخ",
+    filter2: "فاين دايننق",
+    cardOptions: {fixed: 0, highlight: false, showLogo: true}
+  },
+  {
+    jobTitle: "طباخ خبير",
+    companyName: "مطاعم ماكدونلدز",
+    companyLogo: "https://pbs.twimg.com/profile_images/1625792557291651073/GDxGcTsc_400x400.png",
+    location: "الرياض",
+    salary: "50-70 ألف ريال",
+    filter1: "طباخ",
+    filter2: "فاين دايننق",
+    cardOptions: {fixed: 0, highlight: false, showLogo: true}
+  },
+  {
+    jobTitle: "طباخ خبير",
+    companyName: "مطاعم ماكدونلدز",
+    companyLogo: "https://pbs.twimg.com/profile_images/1625792557291651073/GDxGcTsc_400x400.png",
+    location: "الرياض",
+    salary: "50-70 ألف ريال",
+    filter1: "طباخ",
+    filter2: "فاين دايننق",
+    cardOptions: {fixed: 0, highlight: false, showLogo: true}
+  },
+  {
+    jobTitle: "طباخ خبير",
+    companyName: "مطاعم ماكدونلدز",
+    companyLogo: "https://pbs.twimg.com/profile_images/1625792557291651073/GDxGcTsc_400x400.png",
+    location: "الرياض",
+    salary: "50-70 ألف ريال",
+    filter1: "طباخ",
+    filter2: "فاين دايننق",
+    cardOptions: {fixed: 0, highlight: false, showLogo: true}
+  }
+]
+function creatJobCard(element)
+{
+  return(
+    <JobCard 
+      jobTitle={element.jobTitle}
+      companyName={element.companyName}
+      companyLogo={element.companyLogo}
+      jobLocation={element.location}
+      jobSalary={element.salary}
+      filter1={element.filter1}
+      filter2={element.filter2}
+      cardFixed={element.cardOptions.fixed}
+      cardHighlighted={element.cardOptions.cardHighlighted}
+      cardShowLogo={element.cardOptions.showLogo}
+    />
+  )
+}
+function App()
+{
+  return(
+    <div>
+      <div className="postButton">أنشر إعلان وظيفي</div>
+      <div className="cardsContainer">
+        {jobs.map(creatJobCard)}
+      </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/JobCard.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Moment from 'react-moment';
+import 'moment/locale/ar'
+
+function JobCard(props)
+{
+    return(
+        <div className="jobCard" dir="rtl">
+            <div className="logoTableColumn">{props.cardShowLogo ? (<img className="companyLogo" src={props.companyLogo} alt="Company Logo" />):(<img className="companyLogo" src={generateAvatar(props.companyName, 100)} alt="Company Logo" />)}</div>
+           <div className="CompanyNameAndJobTitleContainer">
+               <p className="jobTitle">{props.jobTitle}</p>
+               <p className="companyName">{props.companyName}</p>
+               <div className="locationAndSalaryContainer">{props.jobSalary !== null ? (<p className="salary"><span role="img" aria-label="emoji">💰 </span>{props.jobSalary}</p>) :null}<p className="jobLocation"><span role="img" aria-label="emoji">🌏 </span>{props.jobLocation}</p></div>
+           </div>
+           <div className="filterTags"><p className="filter1">{props.filter1}</p>{props.filter2 !== null ? (<p className="filter2">{props.filter2}</p>) :null}</div>
+           <div className="ApplyButtonTableColumn"><div className="applyButton">تقديم</div></div>
+           <Moment className="timePosted" fromNow ago>2023-02-27T12:59-0500</Moment>
+           <span className="pin" role="img" aria-label="emoji">📌</span>
+        </div>
+      )
+}
+function generateAvatar(username, size) {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    canvas.width = size;
+    canvas.height = size;
+    context.fillStyle = '#FFFFFF'; // Background color
+    context.fillRect(0, 0, size, size);
+    context.font = Math.floor(size / 2) + 'px Arial'; // Font size
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+    context.fillStyle = '#FF0000'; // Text color
+    context.fillText(username.charAt(0).toUpperCase(), size / 2, size / 2);
+    // Draw a circle
+    context.beginPath();
+    context.arc(size / 2, size / 2, size / 2, 0, 2 * Math.PI);
+    return canvas.toDataURL();
+    
+  }
+  
+export default JobCard;
