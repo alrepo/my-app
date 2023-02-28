06ea47b2bfde3f61b28b36c9eb3b8a59cd1be2c7
braintalked <70242053+alrepo@users.noreply.github.com>
2023-02-28 13:58:57 +0300

    auto-scroll to top on page refresh

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-}
 
 ReactDOM.render(
     <App />,
