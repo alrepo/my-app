19de66dbdc9196e0cbc3ccdf5201db8c44d4b840
braintalked <70242053+alrepo@users.noreply.github.com>
2023-03-01 18:36:36 +0300

    added search icon

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FaSearch } from 'react-icons/fa';
 
 function SearchBar() {
   return (
     <div className="search-container" dir="rtl">
       <form>
         <input type="text" placeholder="ابحث بالمسمى الوظيفي..." />
-        <button type="submit"><FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /></button>
+        <button type="submit"><FaSearch className='searchIcon'/></button>
       </form>
       <div className="filter-container">
-        <div className="filter">
-          <select>
-            <option value="">كل الوظائف</option>
-            <option value="full-time">طاهي/شيف</option>
-            <option value="part-time">مدير تشغيل</option>
-            <option value="freelance">باريستا</option>
-          </select>
-        </div>
         <div className="filter">
           <select>
             <option value="كل المناطق">كل المناطق</option>
             <option value="الرياض">الرياض</option>
             <option value="مكة المكرمة">مكة المكرمة</option>
             <option value="المنطقة الشرقية">المنطقة الشرقية</option>
+          </select>
+        </div>
+        <div className="filter">
+          <select>
+            <option value="">كل الوظائف</option>
+            <option value="full-time">طاهي/شيف</option>
+            <option value="part-time">مدير تشغيل</option>
+            <option value="freelance">باريستا</option>
           </select>
         </div>
       </div>
