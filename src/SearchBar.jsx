65b59da5b7c473d3119a4f881003c02753860e3f
braintalked <70242053+alrepo@users.noreply.github.com>
2023-03-02 17:18:58 +0300

    added emojis

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -10,7 +10,7 @@ function SearchBar() {
       <div className="filter-container">
         <div className="filter location">
           <select>
-            <option value="كل المناطق">كل المناطق</option>
+            <option value="كل المناطق"><span role="img" aria-label="emoji">🌏 </span>كل المناطق</option>
             <option value="الرياض">الرياض</option>
             <option value="مكة المكرمة">مكة المكرمة</option>
             <option value="المنطقة الشرقية">المنطقة الشرقية</option>
@@ -18,7 +18,7 @@ function SearchBar() {
         </div>
         <div className="filter classification">
           <select>
-            <option value="كل الفئات">كل الفئات</option>
+            <option value="كل الفئات"><span role="img" aria-label="emoji">🍴 </span>كل الفئات</option>
             <option value="فاين دايننق">فاين دايننق</option>
             <option value="وجبات سريعة">وجبات سريعة</option>
             <option value="أبر كاجوال">أبر كاجوال</option>
@@ -26,13 +26,13 @@ function SearchBar() {
         </div>
         <div className="filter jobNames">
           <select>
-            <option value="">كل الوظائف</option>
+            <option value="كل الوظائف"><span role="img" aria-label="emoji">👨 </span>كل الوظائف</option>
             <option value="full-time">طاهي/شيف</option>
             <option value="part-time">مدير تشغيل</option>
             <option value="freelance">باريستا</option>
           </select>
         </div>
-        <div className="searchButton">بحث</div>
+        <div className="searchButton"><span role="img" aria-label="emoji">🔎 </span>بحث</div>
       </div>
     </div>
   );
