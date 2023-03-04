255ca6a3133cd63b311efb109fbeb4d13fb589db
alikha <kitchenbuy3@gmail.com>
2023-03-04 17:02:05 +0000

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import SearchBar from "./SearchBar";
 
 let jobs = [
   {
-    jobTitle: "طباخ [خبي]ر",
+    jobTitle: "طباخ خبير",
     companyName: "مطاعم ماكدونلدز",
     companyLogo: "https://pbs.twimg.com/profile_images/1625792557291651073/GDxGcTsc_400x400.png",
     location: "المنطقة الشرقية",
